50a3a192a9e5f18d9543a182ec422b2c6c2f7f6b
mmiguez77 <martinmiguez77@gmail.com>
2021-05-07 01:33:54 -0300

    Actualizacion desafio 0705

---
+++ proyectofinal/backend/routes/producto.routes.js
@@ -0,0 +1,41 @@
+import express from 'express';
+const routerProduct = express.Router();
+
+import Producto from '../controllers/Producto.js';
+const producto = new Producto();
+
+
+routerProduct.get('/', producto.viewAll);
+
+/*
+routerProduct.get('/', (req, res) => {
+    const productos = producto.findAll();
+    if (!productos) {
+        return res.status(404).json({ error: 'Productos no encontrado' })
+    } res.json(productos);
+});
+
+
+routerProduct.get('/:id', (req, res) => {
+    const { id } = req.params;
+    const prodFiltro = producto.findByID(id);
+    if (prodFiltro) { return res.json(prodFiltro) };
+    res.status(404).json({ error: 'Producto no encontrado' })
+});
+
+
+routerProduct.put('/:id', (req, res) => {
+    const data = req.body;
+    const { id } = req.params;
+    if (producto.update(id, data)) { res.status(201).json(data) }
+    res.status(400).send();
+})
+
+
+routerProduct.delete('/:id', (req, res) => {
+    const { id } = req.params;
+    producto.delete(id);
+    res.send()
+}) */
+
+export default routerProduct;
