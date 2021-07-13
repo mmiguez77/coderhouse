523a7213a728fe2abe2bd01d87e9b0a9a5dd3237
mmiguez77 <martinmiguez77@gmail.com>
2021-07-13 09:40:16 -0300

    Desafio 25

--- desafio25/controllers/Randoms.js
+++ desafio25/controllers/Randoms.js
@@ -7,15 +7,15 @@ export default class Random {
 
     }
 
-    async getNumber(req, res) {
-        let param = await req.query.number;
+    getNumber(req, res) {
+        let param = req.query.number;
         //console.log(param);
         try {
             const child = fork('../desafio25/helpers/n.js');
             child.send({number: parseInt(param)} );
             
-            child.on('message', async data => await res.send(data));
-            
+            child.on('message', data => res.send(data));
+            setTimeout( () => { child.disconnect() }, 0 );
         } catch (error) {
             console.log(error);
         }
