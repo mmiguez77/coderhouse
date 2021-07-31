de95acae6e65daad6b093b8c488f2d445e57142f
Martin Miguez <martinmiguez77@gmail.com>
2021-07-31 16:58:02 -0300

    Delete pf3entrega directory

--- pf3entrega/config/index.js
+++
@@ -1,9 +0,0 @@
-import dotenv from 'dotenv'
-dotenv.config()
-
-export const FACEBOOK_CLIENT_ID = process.env.FACEBOOK_CLIENT_ID;
-export const FACEBOOK_CLIENT_SECRET = process.env.FACEBOOK_CLIENT_SECRET;
-export const PORT = process.env.PORT;
-
-
-export default PORT;
