1327430bbf3a12cc6b6dc6379259c212941327e9
mmiguez77 <martinmiguez77@gmail.com>
2021-08-17 00:51:16 -0300

    Desafio Clase41 para corrección

--- clase41 - Arquitectura del servidor: Persistencia/src/db_persistence/mensajesPersistence.js
+++ clase41 - Arquitectura del servidor: Persistencia/src/db_persistence/mensajesPersistence.js
@@ -1,31 +1,8 @@
+const BaseMensaje = require ('../repository/mensaje.repository.js');
 const MensajeModel = require('../models/mensajeSchema.js');
-const MongoCxn = require("../database/MongoCxn.js");
 const logger = require('../helpers/winston.js');
 
-class MensajePersistence {
-
-    constructor() {
-        this.cxn = new MongoCxn();
-    }
-
-    async addMsgPersistence(mensaje) {
-        console.log(mensaje);
-        try {
-            const newMsg = await MensajeModel.create(mensaje);
-            return newMsg;
-        } catch (error) {
-            logger.error.error(error);
-        }
-    }
-
-    async findAllMsgPersistence() {
-        try {
-            const mensajes = await MensajeModel.find();
-            return mensajes;
-        } catch (error) {
-            logger.error.error(error);
-        }
-    }
+class MensajePersistence extends BaseMensaje {
 
     async normalizedDataPersistence() {
         try {
